508a99b370d8b201256c6ced0a5d91e00fff7afb
Phillip Denness <dennessphillip@gmail.com>
2018-10-03 19:34:30 +0100

    Update to use NPA

--- src/app/shared/components/hmcts-annotation-ui-lib/annotation-ui-lib.module.ts
+++ src/app/shared/components/hmcts-annotation-ui-lib/annotation-ui-lib.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { CommentsComponent } from './components/comments/comments.component';
 import { CommentItemComponent } from './components/comments/comment-item/comment-item.component';
 import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { AnnotationStoreService } from './data/annotation-store.service';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { PdfAdapter } from './data/store-adapter';
+import { NpaService } from './data/npa.service';
 
 @NgModule({
   imports: [
@@ -19,11 +20,12 @@ import { PdfAdapter } from './data/store-adapter';
     CommentsComponent,
     CommentItemComponent,
     CommentFormComponent,
-    ToolbarComponent
+    ToolbarComponent,
   ],
   providers: [
     AnnotationStoreService,
-    PdfAdapter
+    PdfAdapter,
+    NpaService
   ],
   exports: [
     ToolbarComponent,
