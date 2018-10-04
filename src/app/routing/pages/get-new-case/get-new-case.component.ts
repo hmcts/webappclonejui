4b455094e545b250ef2c8f8fba4ee41128e9af10
padmajaruk <Padmaja.Ramisetti@HMCTS.NET>
2018-10-04 16:16:40 +0100

    RIUI_883_get_case
    -- Get new case component for new page

---
+++ src/app/routing/pages/get-new-case/get-new-case.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {CaseService} from '../../../domain/services/case.service';
+import {FormGroup, FormControl} from "@angular/forms";
+
+@Component({
+    selector: 'get-new-case',
+    templateUrl: './get-new-case.component.html',
+    styleUrls: ['./get-new-case.component.scss']
+})
+export class GetNewCaseComponent implements OnInit {
+    getCaseForm : any;
+    data: Object;
+    error: string;
+
+    constructor(private caseService: CaseService, private router: Router) {
+    }
+
+    ngOnInit() {
+        this.getCaseForm = new FormGroup({
+            getNextCase: new FormControl('')
+        });
+        this.caseService.getJurisdictions().subscribe(val => { console.log(val); this.data = val});
+        console.log("data ", this.data);
+    }
+
+
+    onSubmit() {
+        this.router.navigate([''], {
+            queryParams: { selectedJurisdiction : this.getCaseForm.value.getNextCase}
+        });
+    }
+}
